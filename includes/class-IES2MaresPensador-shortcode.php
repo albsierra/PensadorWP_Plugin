669809efefacd9093e2ab48ab3ef825a70af3ace
Alberto Sierra <alberto.sierra@murciaeduca.es>
2018-02-18 10:55:14 +0100

    formateando la visualización de los retos

--- includes/class-IES2MaresPensador-shortcode.php
+++ includes/class-IES2MaresPensador-shortcode.php
@@ -41,12 +41,23 @@ class IES2MaresPensador_shortcode
             $query = new WP_Query( $args);
             ob_start();
             if ( $query->have_posts() ) : ?>
+                <ul class="cards">
                 <?php while ( $query->have_posts() ) : $query->the_post(); ?>
-                    <div>
-                        <h2><?php the_title(); ?></h2>
-                        <?php the_content(); ?>
-                    </div>
+                        <li class="card">
+                            <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+                                <?php the_title('<h1>','</h1>'); ?>
+                            </a>
+                            <?php
+                            $departamentoId = @get_post_meta(get_the_ID(), 'departamento', true);                                $fechaFin = @get_post_meta(get_the_ID(), 'fecha_fin', true);
+                            $departamento = get_term($departamentoId, 'category');
+
+                            $destinatarios = @get_post_meta(get_the_ID(), 'destinatarios', true);
+                            ?>
+                            <p style="display:inline; float:right"><?php echo $departamento->name; ?></p>
+                            <p><?php echo $destinatarios ?></p>
+                        </li>
                 <?php endwhile; wp_reset_postdata(); ?>
+                </ul>
                 <!-- show pagination here -->
             <?php else : ?>
                 <!-- show 404 error here -->
